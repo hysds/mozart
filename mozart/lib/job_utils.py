7a6eb160ebe1b7a17e8b44e1aebb2ee3f73f0dab
DustinKLo <dustinlo.pcc@gmail.com>
2020-02-20 13:33:54 -0800

    removed mongo fron job_utils.py

--- mozart/lib/job_utils.py
+++ mozart/lib/job_utils.py
@@ -5,31 +5,29 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 
-import requests
 import json
-from pymongo import MongoClient
 import hysds_commons.request_utils
 
 from mozart import app
 
 
-def get_job_status(ident):
+def get_job_status(_id):
     """
     Get the status of a job with the given identity
-    @param ident - id of the job
+    @param _id - id of the job
     """
-    if ident is None:
+    if _id is None:
         raise Exception("'id' must be supplied by request")
 
     es_url = app.config['ES_URL']
     es_index = "job_status-current"
-    full_url = "{0}/{1}/job/{2}?_source=status".format(es_url, es_index, ident)
+    full_url = "{0}/{1}/job/{2}?_source=status".format(es_url, es_index, _id)
     return hysds_commons.request_utils.get_requests_json_response(full_url)["_source"]["status"]
 
 
 def get_job_list():
     """Get a listing of jobs"""
-    # query
+
     data = {"query": {"match_all": {}}, "fields": []}
     es_url = app.config['ES_URL']
     es_index = "job_status-current"
@@ -51,49 +49,3 @@ def get_job_info(ident):
     es_index = "job_status-current"
     full_url = "{0}/{1}/job/{2}".format(es_url, es_index, ident)
     return hysds_commons.request_utils.get_requests_json_response(full_url)["_source"]
-
-###
-# Historic Job Util functions
-#
-###
-
-
-def get_mongo_client():
-    """Return MongoDB client."""
-
-    return MongoClient('mongodb://localhost/')
-
-
-def get_db():
-    """Return mozart MongoDB database."""
-
-    return get_mongo_client()['mozart']
-
-
-def get_execute_nodes():
-    """Return the names of all execute nodes."""
-
-    query = {
-        "size": 0,
-        "facets": {
-            "job.job_info.execute_node": {
-                "terms": {
-                    "field": "job.job_info.execute_node",
-                    "all_terms": True
-                }
-            }
-        }
-    }
-    es_url = app.config['ES_URL']
-    index = app.config['JOB_STATUS_INDEX']
-    r = requests.post('%s/%s/_search' %
-                      (es_url, index), data=json.dumps(query))
-    r.raise_for_status()
-    result = r.json()
-    # app.logger.debug(pformat(result))
-    total = len(result['facets']['job.job_info.execute_node']['terms'])
-    nodes = []
-    for terms in result['facets']['job.job_info.execute_node']['terms']:
-        nodes.append(terms['term'])
-    nodes.sort()
-    return nodes
