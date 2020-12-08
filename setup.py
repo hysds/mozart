de7a9108d4476fba159eb54a6ff4f2b8feaa9a80
Gerald Manipon <pymonger@gmail.com>
2020-12-08 10:39:36 -0800

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozart',
-    version='2.0.7',
+    version='2.0.8',
     long_description='HySDS job orchestration/worker web interface',
     packages=find_packages(),
     include_package_data=True,
