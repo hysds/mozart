2db6475b278920770a81d030ac377ebb9763162f
dustinklo <dustinlo.pcc@gmail.com>
2020-12-09 15:24:13 -0800

    changed execute command (using with now) in jenkins.py added logger as well

--- mozart/services/jenkins.py
+++ mozart/services/jenkins.py
@@ -9,7 +9,8 @@ standard_library.install_aliases()
 
 from flask import Blueprint, Response, request
 from flask_restx import Api, apidoc, Resource, fields
-import subprocess
+
+from subprocess import Popen, PIPE, STDOUT
 
 from mozart import app
 
@@ -39,25 +40,26 @@ class JenkinsJob(Resource):
 
     @staticmethod
     def execute(cmd):
-        prcss = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True, shell=True)
-        for stdout_line in iter(prcss.stdout.readline, ""):
-            if 'GIT_OAUTH_TOKEN' not in stdout_line and '@github.com' not in stdout_line:
-                # TODO: this is a workaround to mask the github oauth token, need to find a better way to do this
-                app.logger.debug(stdout_line)
-                yield 'data: ' + stdout_line + '\n'
-        prcss.stdout.close()
+        with Popen(cmd, stdout=PIPE, stderr=STDOUT, universal_newlines=True, bufsize=1, shell=True) as p:
+            for stdout_line in iter(p.stdout.readline, ''):
+                if 'GIT_OAUTH_TOKEN' not in stdout_line and '@github.com' not in stdout_line:
+                    # TODO: this is a workaround to mask the github oauth token, need to find a better way to do this
+                    yield 'data: ' + stdout_line + '\n'
+            p.stdout.close()
 
     def put(self):
         """Register jobs in jenkins"""
         request_data = request.json or request.form
         repo = request_data.get('repo')
         branch = request_data.get('branch')
+        app.logger.info("repo: %s" % repo)
+        app.logger.info("branch: %s" % branch)
 
         if repo is None:
             return {'success': False, 'message': 'repo must be supplied'}, 400
 
         if branch:
-            cmd = 'exec sds -d ci add_job -b %s --token %s s3' % (branch, repo)
+            cmd = 'exec sds -d ci add_job -b %s --token %s s3' % (branch, repo)  # exec
         else:
             cmd = 'exec sds -d ci add_job --token %s s3' % repo
         resp = Response(self.execute(cmd), mimetype="text/event-stream")
@@ -71,12 +73,14 @@ class JenkinsJob(Resource):
         """build jobs in jenkins (using EventSource javascript api)"""
         repo = request.args.get('repo')
         branch = request.args.get('branch')
+        app.logger.info("repo: %s" % repo)
+        app.logger.info("branch: %s" % branch)
 
         if repo is None:
             return {'success': False, 'message': 'repo must be supplied'}, 400
 
         if branch:
-            cmd = 'exec sds -d ci build_job -b %s %s' % (branch, repo)
+            cmd = 'exec sds -d ci build_job -b %s %s' % (branch, repo)  # exec
         else:
             cmd = 'exec sds -d ci build_job %s' % repo
         resp = Response(self.execute(cmd), mimetype="text/event-stream")
@@ -91,12 +95,14 @@ class JenkinsJob(Resource):
         request_data = request.json or request.form
         repo = request_data.get('repo')
         branch = request_data.get('branch')
+        app.logger.info("repo: %s" % repo)
+        app.logger.info("branch: %s" % branch)
 
         if repo is None:
             return {'success': False, 'message': 'repo must be supplied'}, 400
 
         if branch:
-            cmd = 'exec sds -d ci build_job -b %s %s' % (branch, repo)
+            cmd = 'exec sds -d ci build_job -b %s %s' % (branch, repo)  # exec
         else:
             cmd = 'exec sds -d ci build_job %s' % repo
         resp = Response(self.execute(cmd), mimetype="text/event-stream")
