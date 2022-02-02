842abac235a79aa0cff805b7adffdab5f525eb09
Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
2022-02-02 10:09:21 -0800

    HC-367: Update to latest Pallet Project versions (#39)
    * added Flask>2.0.0,<3.0.0, updated flask-restx, flask-WTF, gunicorn
    
    * commented out click b/c its not being used
    
    * Sync Flask pins to pele
    
    * Bump version
    
    Co-authored-by: dustinklo <dustinlo.pcc@gmail.com>
    Co-authored-by: Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>

--- setup.py
+++ setup.py
@@ -9,22 +9,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozart',
-    version='2.0.14',
+    version='2.0.15',
     long_description='HySDS job orchestration/worker web interface',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        # TODO: remove this pin on click once this celery issue is resolved:
-        # https://github.com/celery/celery/issues/6768
-        'click>=7.0,<8.0',
-        # TODO: remove these pins on flask/extensions once the celery issue above is resolved
-        'flask-restx>=0.4.0',
-        'Flask-SQLAlchemy>=2.5.1,<3.0.0',
-        'Flask-WTF>=0.14.3,<1.0.0',
-        'Flask-DebugToolbar>=0.11.0,<1.0.0',
-        'Flask-Login>=0.5.0,<1.0.0',
-        'gunicorn>=19.1.1',
+        'Flask>=2.0.0',
+        'flask-restx>=0.5.1',
+        'Flask-SQLAlchemy>=2.5.1',
+        'Flask-WTF>=0.15.1',
+        'Flask-DebugToolbar>=0.11.0',
+        'Flask-Login>=0.5.0',
+        'gunicorn>=20.1.0',
         'gevent>=1.1.1',
         'eventlet>=0.17.4',
         'requests>=2.7.0',
