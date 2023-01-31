5f4bea98a44176602ddfc79adaa0fc41bf15ade1
Dustin Lo <dustinlo.pcc@gmail.com>
2023-01-31 10:12:26 -0800

    updated Flask-SQLAlchemy, Flask + werkzeug (#41)
    db_create.py is now using the app context when creating the database
    
    Co-authored-by: dustinlo <dustin.k.lo@jpl.nasa.gov>

--- setup.py
+++ setup.py
@@ -9,21 +9,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozart',
-    version='2.0.16',
+    version='2.0.17',
     long_description='HySDS job orchestration/worker web interface',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Flask>=2.0.0',
+        'Flask>=2.2.0',
         'flask-restx>=0.5.1',
-        'Flask-SQLAlchemy>=2.5.1',
+        'Flask-SQLAlchemy>=3.0.0',
         'Flask-WTF>=0.15.1',
         'Flask-DebugToolbar>=0.11.0',
         'Flask-Login>=0.5.0',
         'gunicorn>=20.1.0',
         'gevent>=1.1.1',
-        'eventlet>=0.17.4',
+        'eventlet>=0.33.3',
         'requests>=2.7.0',
         'simpleldap>=0.8',
         'simplekml>=1.2.3',
@@ -37,6 +37,6 @@ setup(
         'future>=0.17.1',
         'pytz',
         'numpy',
-        "werkzeug==2.1.2",  # TODO: remove this pin after fix has been made https://stackoverflow.com/a/73105878
+        "werkzeug>=2.2.0",  # TODO: remove this pin after fix has been made https://stackoverflow.com/a/73105878
     ]
 )
