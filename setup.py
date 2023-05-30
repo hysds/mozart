d838e4fe8ee971fbd2eb13227456310c232817c3
Dustin Lo <dustinlo.pcc@gmail.com>
2023-05-30 11:55:43 -0700

    fix empty logging, bump version (#51)
    Co-authored-by: dustinlo <dustin.k.lo@jpl.nasa.gov>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozart',
-    version='2.2.2',
+    version='2.2.3',
     long_description='HySDS job orchestration/worker web interface',
     packages=find_packages(),
     include_package_data=True,
