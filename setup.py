8bec0cf3d5c8ac0f68c3f62fd9ac5763c3ad9660
Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
2023-09-27 17:31:38 -0700

    HC-496: Updates to optionally support a separate Mozart ES cluster deployment (#53)
    * Installation of ILM templates and policy occurs through scripts
    
    * fix template
    
    * update
    
    * bump version
    
    * Add newline; add argument flag to change policy name
    
    * fixed typo in name
    
    ---------
    
    Co-authored-by: Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozart',
-    version='2.2.4',
+    version='2.2.5',
     long_description='HySDS job orchestration/worker web interface',
     packages=find_packages(),
     include_package_data=True,
