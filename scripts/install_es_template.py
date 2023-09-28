a83aaa4a2c5d773137a5aa576179cdf62fc43389
Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
2023-09-27 17:31:38 -0700

    HC-496: Updates to optionally support a separate Mozart ES cluster deployment (#53)
    * Installation of ILM templates and policy occurs through scripts
    
    * fix template
    
    * update
    
    * bump version
    
    * Add newline; add argument flag to change policy name
    
    * fixed typo in name
    
    ---------
    
    Co-authored-by: Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>

--- scripts/install_es_template.py
+++ scripts/install_es_template.py
@@ -7,6 +7,7 @@ from builtins import open
 from future import standard_library
 standard_library.install_aliases()
 
+import argparse
 import os
 from jinja2 import Template
 
@@ -14,6 +15,25 @@ from hysds.es_util import get_mozart_es
 
 
 mozart_es = get_mozart_es()
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="Tool to install Mozart ES templates to the cluster.")
+    parser.add_argument(
+        "--install_job_templates",
+        required=False,
+        action="store_true",
+        default=False,
+        help="Optionally specify this flag to install the job templates (job_status, worker_status, "
+             "event_status, task_status).",
+    )
+    parser.add_argument(
+        "--template_dir",
+        required=False,
+        type=str,
+        help="Optionally specify this flag to tell the tool where to find the given templates.",
+    )
+    return parser
 
 
 def write_template(index, tmpl_file):
@@ -30,12 +50,45 @@ def write_template(index, tmpl_file):
     print("Successfully installed template %s" % index)
 
 
+def write_index_template(index, tmpl_file):
+    """Write template to ES."""
+
+    with open(tmpl_file) as f:
+        tmpl = Template(f.read()).render(index=index)
+
+    # https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.client.IndicesClient.delete_template
+    mozart_es.es.indices.delete_index_template(name=index, ignore=[400, 404])
+
+    # https://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.client.IndicesClient.put_template
+    mozart_es.es.indices.put_index_template(name=index, body=tmpl)
+    print("Successfully installed index template %s" % index)
+
+
 if __name__ == "__main__":
-    indices = ("containers", "job_specs", "hysds_ios")
+    args = get_parser().parse_args()
 
-    curr_file = os.path.dirname(__file__)
-    tmpl_file = os.path.abspath(os.path.join(curr_file, '..', 'configs', 'es_template.json'))
-    tmpl_file = os.path.normpath(tmpl_file)
+    if args.install_job_templates is False:
+        indices = ("containers", "job_specs", "hysds_ios")
 
-    for index in indices:
-        write_template(index, tmpl_file)
+        curr_file = os.path.dirname(__file__)
+        tmpl_file = os.path.abspath(os.path.join(curr_file, '..', 'configs', 'es_template.json'))
+        tmpl_file = os.path.normpath(tmpl_file)
+
+        for index in indices:
+            write_template(index, tmpl_file)
+    else:
+        templates = [
+            "job_status.template",
+            "worker_status.template",
+            "task_status.template",
+            "event_status.template"
+        ]
+        if args.template_dir is None:
+            raise RuntimeError(f"Must specify --template_dir when installing job templates.")
+
+        for template in templates:
+            # Copy templates to etc/ directory
+            template_file = f"{args.template_dir}/{template}"
+            template_doc_name = template.split(".template")[0]
+            print(f"Creating ES index template for {template}")
+            write_index_template(template_doc_name, template_file)
