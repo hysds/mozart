ff3c02454e7774d9b881d39d400ddb649419383f
Joshua Garde <jgarde@jpl.nasa.gov>
2024-03-14 16:08:13 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mozart',
-    version='2.3.0',
+    version='2.3.1',
     long_description='HySDS job orchestration/worker web interface',
     packages=find_packages(),
     include_package_data=True,
